f9e7261fa342803510a1fda19ec318b9352bcfcd
dnassehi <damoun.nassehi@gmail.com>
2025-07-29 23:29:26 +0200

    add base url to app.yaml and src/router.tsx and app.config.ts

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export function createRouter() {
     scrollRestoration: true,
     defaultPreload: "intent",
     defaultPendingComponent: () => <div>Loading...</div>,
+    basepath: "/",
   });
 
   return router;
